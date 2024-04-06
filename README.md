dfb6f6fd2734a1463dab19bed43a8105258be61f
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-04-06 15:06:20 -0400

    Backup crawler

---
+++ README.md
@@ -0,0 +1,18 @@
+# README
+
+Web Crawler to get all images from a web site given a URL.
+
+## About this
+
+This Web Crawler was developed using following technologies:
+
+- NodeJS
+- TypesScript by using the following dependency _ts-node_
+- _node-fetch_ a module to use fetch in NodeJS, nevertheless, fetch is out of the box since Node version 18
+- _cheerio_ as a dependency to handle DOM data and make queries similar to jQuery
+
+## References
+
+- [https://github.com/codyseibert/youtube/tree/master/web-crawler-node](https://github.com/codyseibert/youtube/tree/master/web-crawler-node)
+
+Some small fixes were made over the original for example, adding the tsconfig.json since some errors due to TS were marked.
---
+++ src/index.ts
@@ -0,0 +1,63 @@
+import * as fetch from "node-fetch";
+import * as cheerio from "cheerio";
+import * as fs from "fs";
+import * as path from "path";
+import * as urlParser from "url";
+
+const seenUrls = {} as any;
+
+const getUrl = (link:string, host:string, protocol:string) => {
+  if (link.includes("http")) {
+    return link;
+  } else if (link.startsWith("/")) {
+    return `${protocol}//${host}${link}`;
+  } else {
+    return `${protocol}//${host}/${link}`;
+  }
+};
+
+const crawl = async ({ url, ignore }:{url:string, ignore:string}) => {
+  if (seenUrls[url]) return;
+  console.log("crawling", url);
+  seenUrls[url] = true;
+
+  const parsedUrl = urlParser.parse(url);
+
+  if ( parsedUrl.host && typeof parsedUrl.host === "string" && parsedUrl.protocol && typeof parsedUrl.protocol === "string" ) {
+    const { host, protocol } = parsedUrl as { host:string, protocol: string }
+    const response = await fetch(url);
+    const html = await response.text();
+    const $ = cheerio.load(html);
+  const links = $("a")
+    .map((i, link) => link.attribs.href)
+    .get();
+
+  const imageUrls = $("img")
+    .map((i, link) => link.attribs.src)
+    .get();
+
+  imageUrls.forEach((imageUrl) => {
+    fetch(getUrl(imageUrl, host, protocol)).then((response) => {
+      const filename = path.basename(imageUrl);
+      const dest = fs.createWriteStream(`images/${filename}`);
+      response.body.pipe(dest);
+    });
+  });
+
+  links
+    .filter((link) => link.includes(host) && !link.includes(ignore))
+    .forEach((link) => {
+      crawl({
+        url: getUrl(link, host, protocol),
+        ignore,
+      });
+    });
+  } else {
+    console.error("Invalid URL format")
+  }
+};
+
+crawl({
+  url: "http://stevescooking.blogspot.com/",
+  ignore: "/search",
+});
