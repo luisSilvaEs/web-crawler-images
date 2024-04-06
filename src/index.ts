07d2cc6debd9d88be919b023fecc5486ef1414cf
Luis Silva <luissilva@Luiss-MacBook-Air.local>
2024-04-06 19:50:24 -0400

    Add prettier to format code

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,68 @@
-import * as fetch from "node-fetch";
-import * as cheerio from "cheerio";
-import * as fs from "fs";
-import * as path from "path";
-import * as urlParser from "url";
+import * as fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as urlParser from 'url';
 
 const seenUrls = {} as any;
 
-const getUrl = (link:string, host:string, protocol:string) => {
-  if (link.includes("http")) {
+const getUrl = (link: string, host: string, protocol: string) => {
+  if (link.includes('http')) {
     return link;
-  } else if (link.startsWith("/")) {
+  } else if (link.startsWith('/')) {
     return `${protocol}//${host}${link}`;
   } else {
     return `${protocol}//${host}/${link}`;
   }
 };
 
-const crawl = async ({ url, ignore }:{url:string, ignore:string}) => {
+const crawl = async ({ url, ignore }: { url: string; ignore: string }) => {
   if (seenUrls[url]) return;
-  console.log("crawling", url);
+  console.log('crawling', url);
   seenUrls[url] = true;
 
   const parsedUrl = urlParser.parse(url);
 
-  if ( parsedUrl.host && typeof parsedUrl.host === "string" && parsedUrl.protocol && typeof parsedUrl.protocol === "string" ) {
-    const { host, protocol } = parsedUrl as { host:string, protocol: string }
+  if (
+    parsedUrl.host &&
+    typeof parsedUrl.host === 'string' &&
+    parsedUrl.protocol &&
+    typeof parsedUrl.protocol === 'string'
+  ) {
+    const { host, protocol } = parsedUrl as { host: string; protocol: string };
     const response = await fetch(url);
     const html = await response.text();
     const $ = cheerio.load(html);
-  const links = $("a")
-    .map((i, link) => link.attribs.href)
-    .get();
+    const links = $('a')
+      .map((i, link) => link.attribs.href)
+      .get();
 
-  const imageUrls = $("img")
-    .map((i, link) => link.attribs.src)
-    .get();
+    const imageUrls = $('img')
+      .map((i, link) => link.attribs.src)
+      .get();
 
-  imageUrls.forEach((imageUrl) => {
-    fetch(getUrl(imageUrl, host, protocol)).then((response) => {
-      const filename = path.basename(imageUrl);
-      const dest = fs.createWriteStream(`images/${filename}`);
-      response.body.pipe(dest);
-    });
-  });
-
-  links
-    .filter((link) => link.includes(host) && !link.includes(ignore))
-    .forEach((link) => {
-      crawl({
-        url: getUrl(link, host, protocol),
-        ignore,
+    imageUrls.forEach((imageUrl) => {
+      fetch(getUrl(imageUrl, host, protocol)).then((response) => {
+        const filename = path.basename(imageUrl);
+        const dest = fs.createWriteStream(`images/${filename}`);
+        response.body.pipe(dest);
       });
     });
+
+    links
+      .filter((link) => link.includes(host) && !link.includes(ignore))
+      .forEach((link) => {
+        crawl({
+          url: getUrl(link, host, protocol),
+          ignore,
+        });
+      });
   } else {
-    console.error("Invalid URL format")
+    console.error('Invalid URL format');
   }
 };
 
 crawl({
-  url: "http://stevescooking.blogspot.com/",
-  ignore: "/search",
+  url: 'http://stevescooking.blogspot.com/',
+  ignore: '/search',
 });
